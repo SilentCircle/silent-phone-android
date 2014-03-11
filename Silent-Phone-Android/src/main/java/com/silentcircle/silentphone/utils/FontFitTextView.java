950e0d3c76b7f592a032010580230ca2dae85b43
mDeSilvio <michaeldesilvio@gmail.com>
2014-03-11 14:11:57 -0400

    migrate from ADT (Ellipse) to SDK (IntelliJ) w/gradle v1.11

---
+++ Silent-Phone-Android/src/main/java/com/silentcircle/silentphone/utils/FontFitTextView.java
@@ -0,0 +1,122 @@
+/*
+Copyright © 2012-2013, Silent Circle, LLC.  All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Any redistribution, use, or modification is done solely for personal 
+      benefit and not for any commercial purpose or for monetary gain
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name Silent Circle nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL SILENT CIRCLE, LLC BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package com.silentcircle.silentphone.utils;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.widget.EditText;
+
+public class FontFitTextView extends EditText {
+
+    private float textSize;
+    private boolean heightSet;
+    int useHeight;
+    
+    public FontFitTextView(Context context) {
+        super(context);
+        initialise();
+    }
+
+    public FontFitTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialise();
+    }
+
+    private void initialise() {
+        mTestPaint = new Paint();
+        mTestPaint.set(this.getPaint());
+        //max size defaults to the initially specified text size unless it is too small
+        textSize = getTextSize();
+    }
+
+    /* Re size the font so the specified text fits in the text box
+     * assuming the text box is the specified width.
+     */
+    private void refitText(String text, int textWidth) 
+    { 
+        if (textWidth <= 0)
+            return;
+        int targetWidth = textWidth - this.getTotalPaddingLeft() - this.getTotalPaddingRight();
+        float hi = textSize;
+        float lo = textSize / 2;
+        final float threshold = 0.5f; // How close we have to be
+
+        mTestPaint.set(this.getPaint());
+
+        while((hi - lo) > threshold) {
+            float size = (hi+lo)/2;
+            mTestPaint.setTextSize(size);
+            if (mTestPaint.measureText(text) >= targetWidth) 
+                hi = size; // too big
+            else
+                lo = size; // too small
+        }
+        // Use lo so that we under shoot rather than overshoot
+        // Also use Pixel, not SP because here we are already working with scaled real pixel values
+        this.setTextSize(TypedValue.COMPLEX_UNIT_PX, lo);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int height = heightSet ? useHeight : getMeasuredHeight();
+        refitText(this.getText().toString(), parentWidth);
+        this.setMeasuredDimension(parentWidth, height);
+    }
+
+    @Override
+    protected void onTextChanged(final CharSequence text, final int start, final int before, final int after) {
+        refitText(text.toString(), this.getWidth());
+    }
+
+    /**
+     * Recompute text size if necessary.
+     * 
+     * On first call to size changed old width is not equal to current width, thus
+     * recompute and remember the current measured height. The height should stay at
+     * this value even if the text size shrinks.
+     */
+    @Override
+    protected void onSizeChanged (int w, int h, int oldw, int oldh) {
+        if (w != oldw) {
+            refitText(this.getText().toString(), w);
+            if (!heightSet) {
+                useHeight = getMeasuredHeight();
+                heightSet = true;
+            }
+        }
+    }
+
+    //Attributes
+    private Paint mTestPaint;
+}
